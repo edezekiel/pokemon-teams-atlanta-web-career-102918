1120d8bd16df3e28550d9b371b2919e0c98df8df
Edward Ezekiel <ed.a.ezekiel@gmail.com>
2018-12-31 09:06:53 -0700

    create the initPage function and getTrainers function

--- pokemon-teams-frontend/src/index.js
+++ pokemon-teams-frontend/src/index.js
@@ -1,3 +1,14 @@
 const BASE_URL = "http://localhost:3000"
 const TRAINERS_URL = `${BASE_URL}/trainers`
 const POKEMONS_URL = `${BASE_URL}/pokemons`
+
+document.addEventListener("DOMContentLoaded", initPage)
+
+function initPage(event) {
+  getTrainers()
+}
+function getTrainers(event) {
+  fetch(TRAINERS_URL)
+  .then(res => res.json())
+  .then(console.log)
+}
